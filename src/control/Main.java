0c7fe6f7686a3c3d96a28828f97c8198198e77e6
FranciscoSosa <FranciscoSosa>
2021-12-29 17:15:12 +0000

    Kata6 - versión 2

--- src/control/Main.java
+++ src/control/Main.java
@@ -1,6 +1,7 @@
 package control;
 
 import toys.Car;
+import toys.Helicopter;
 import toys.SerialNumberGenerator;
 
 import java.util.ArrayList;
@@ -12,20 +13,39 @@ public class Main {
     public static void main(String[] args) {
         SerialNumberGenerator generator = new SerialNumberGenerator();
         List<Car> cars = new ArrayList<>();
+        List<Helicopter> helicopters = new ArrayList<>();
         Scanner in = new Scanner(System.in);
         String line = "";
         while(!"exit".equals(line)){
             line = in.nextLine();
-            if(!"exit".equals(line)){
-                Car car = new Car(generator.next());
-                car.pack();
-                car.label();
-                cars.add(car);
-                System.out.println("Built cars " + cars
-                        .stream()
-                        .map(c -> c.getSerialNumber().toString())
-                        .collect(Collectors.joining(", "))
-                );
+            switch(line){
+                case "car":
+                    Car car = new Car(generator.next());
+                    car.pack();
+                    car.label();
+                    cars.add(car);
+                    System.out.println("Built cars " + cars
+                            .stream()
+                            .map(c -> c.getSerialNumber().toString())
+                            .collect(Collectors.joining(", "))
+                    );
+                    break;
+                case "helicopter":
+                    Helicopter helicopter = new Helicopter(generator.next());
+                    helicopter.pack();
+                    helicopter.label();
+                    helicopters.add(helicopter);
+                    System.out.println("Built helicopters " + helicopters
+                            .stream()
+                            .map(h -> h.getSerialNumber().toString())
+                            .collect(Collectors.joining(", "))
+                    );
+                    break;
+                case "exit":
+                    System.out.println("Exiting...");
+                    break;
+                default:
+                    System.out.println("Command unknown!");
             }
         }
     }
